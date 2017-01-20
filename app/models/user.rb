b1ab5ccb35b8a50957d9cc134039dfc7e6ff96d8
Tom Modeste <tom.modeste@gmail.com>
2017-01-20 12:23:18 +0100

    fully functionnal auth system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :fname, presence: true, length: { maximum: 30 }
+  validates :lname, presence: true, length: { maximum: 30 }
+  validates :address, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
